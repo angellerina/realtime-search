f37907efc95e12e86610716d69678f3bb1944fb0
angelle rina <angellerinac@gmail.com>
2020-08-12 23:44:23 -0400

    Updates

---
+++ precache-manifest.2b5e8be281c1cd105b98a14d857c0623.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77325a82ac85c3c347c564e12837d78e",
+    "url": "/realtime-search/index.html"
+  },
+  {
+    "revision": "ade99019d201aca8a79e",
+    "url": "/realtime-search/static/css/main.ad460cd1.chunk.css"
+  },
+  {
+    "revision": "e647111a7c70a6455304",
+    "url": "/realtime-search/static/js/2.23c507a3.chunk.js"
+  },
+  {
+    "revision": "e1467b9dee3949b845d42738be528a1c",
+    "url": "/realtime-search/static/js/2.23c507a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ade99019d201aca8a79e",
+    "url": "/realtime-search/static/js/main.4e7acaa3.chunk.js"
+  },
+  {
+    "revision": "725b6eaf3e1cd7a62ee1",
+    "url": "/realtime-search/static/js/runtime-main.bfcc9162.js"
+  }
+]);
